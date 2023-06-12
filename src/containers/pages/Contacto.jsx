2f08bbb1705ab22ee204c542621288859cd1e732
imrtr <ahzbs@gmail.com>
2023-06-12 04:10:54 -0600

    framerMotion

--- src/containers/pages/Contacto.jsx
+++ src/containers/pages/Contacto.jsx
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet-async';
+
 import Footer from "../../components/navigation/Footer"
 import Navbar from "../../components/navigation/Navbar"
 import Layout from "../../hocs/layouts/Layout"
@@ -6,6 +8,14 @@ import Contactoo from "../../components/sections/Contacto"
 const Contacto = () =>{
     return (
         <Layout>
+            <Helmet>
+                <title>Blocksidian | Contact</title>
+                <meta name='description' content='Buy and sell tickets with solidity' />
+                <meta name='keywords' content='blocksidian, blockchain' />
+                <meta name='robots' content="all" />
+                <meta name='author' content='Blocksidian' />
+                <meta name='publisher' content='Blocksidian' />
+            </Helmet>
             <Navbar />
                 <Contactoo />
             <Footer />
--- src/containers/pages/Home.jsx
+++ src/containers/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet-async';
+
 import Footer from "../../components/navigation/Footer"
 import Navbar from "../../components/navigation/Navbar"
 import Layout from "../../hocs/layouts/Layout"
@@ -7,10 +9,18 @@ import About from "../../components/navigation/About"
 import Steps from "../../components/navigation/Steps"
 import Video from "../../components/navigation/videoPresentation"
 
-
 const Home = () =>{
     return (
         <Layout>
+            <Helmet>
+                <title>Blocksidian | Home</title>
+                <meta name='description' content='Buy and sell tickets with solidity' />
+                <meta name='keywords' content='blocksidian, blockchain' />
+                <meta name='robots' content="all" />
+                <meta name='author' content='Blocksidian' />
+                <meta name='publisher' content='Blocksidian' />
+            </Helmet>
+
             <div>
                 <Navbar />
                 <HeroSection />
