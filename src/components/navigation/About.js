ebca3958f3e531802be244d748fee9cd2a844eca
imrtr <ahzbs@gmail.com>
2023-06-13 03:25:00 -0600

    restore

--- src/components/navigation/About.js
+++ src/components/navigation/About.js
@@ -3,24 +3,9 @@ import { Fade } from "@successtar/react-reveal";
 
 export default function About() {
   return (
-<<<<<<< HEAD
-    <section className="overflow-hidden bg-white dark:bg-slatee-900 py-20 sm:grid sm:grid-cols-2">
-      <div className="relative">
-        <Fade duration={2000}>
-        <img
-          alt="Student"
-          src="https://images.unsplash.com/photo-1464582883107-8adf2dca8a9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-          className="mx-auto md:ml-8 h-full w-80 md:w-full object-cover"
-        />
-        </Fade>
-      </div>
-      <div className="p-8 md:p-12 lg:px-16 lg:py-24">
-        <div className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-=======
-    <section className="overflow-hidden bg-whitee-900 dark:bg-slatee-900 py-20 -mt-28 sm:-mt-32 sm:-mb-52">
-      <div className="p-8 md:p-12 lg:px-16 lg:py-24 w-5/6 m-auto">
-        <div className="mx-auto text-center ltr:sm:text-left rtl:sm:text-right">
->>>>>>> staging
+    <section className="overflow-hidden bg-whitee-900 dark:bg-slatee-900 py-20">
+      <div className="p-8 md:p-12 lg:px-16 lg:py-24 mx-auto w-4/5">
+        <div className="text-center ltr:sm:text-left rtl:sm:text-right">
           <Fade duration={2000}>
             <h2 className="text-2xl font-bold text-gray-900 dark:text-whitee-900 md:text-4xl">
               About
